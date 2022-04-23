be2b35ec37628a9a096287a197fee57c5536d1d3
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-04-23 14:08:47 +0300

    added db package

--- cmd/go-postgresql-crud-example/config.go
+++ cmd/go-postgresql-crud-example/config.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/perfectgentlemande/go-postgresql-crud-example/internal/api"
+	"github.com/perfectgentlemande/go-postgresql-crud-example/internal/database/dbuser"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	DBUser *dbuser.Config `yaml:"dbuser"`
+	Server *api.Config    `yaml:"server"`
+}
+
+func readConfig(fileName string) (*Config, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	config := &Config{}
+	err = yaml.NewDecoder(file).Decode(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode: %w", err)
+	}
+
+	return config, nil
+}
